d46c159959c7bd098116615cf87fccb290cd048d
saipradeepmorla <morlasaipradeep@gmail.com>
2024-10-23 23:06:02 +0530

    fixed footer nav links and other links and minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,38 @@ import {
   FaLinkedinIn,
   FaTwitter,
   FaGithub,
+  FaLocationArrow,
 } from "react-icons/fa";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
+  const navLinks = [
+    {
+      id: 1,
+      link: "",
+      text: "Home",
+    },
+    {
+      id: 2,
+      link: "services",
+      text: "Services",
+    },
+    {
+      id: 3,
+      link: "courses",
+      text: "Courses",
+    },
+    {
+      id: 4,
+      link: "aboutus",
+      text: "About Us",
+    },
+    {
+      id: 5,
+      link: "contact",
+      text: "Contact",
+    },
+  ];
   const socialMediaData = [
     {
       icon: FaFacebookF,
@@ -34,7 +62,7 @@ const Footer = () => {
   ];
 
   return (
-    <footer className="px-4 py-12 text-gray-300 bg-gray-800 sm:px-6 lg:px-8">
+    <footer className="px-4 pt-6 pb-4 md:pt-12 text-gray-300 bg-gray-800 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-7xl">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
           <div className="lg:col-span-1">
@@ -52,18 +80,16 @@ const Footer = () => {
                 Navigation
               </h3>
               <ul className="space-y-2">
-                {["home", "services", "courses", "about us", "contact us"].map(
-                  (item) => (
-                    <li key={item}>
-                      <Link
-                        to={`/${item.replace(" ", "-")}`}
-                        className="text-sm transition-colors duration-300 hover:text-white"
-                      >
-                        {item}
-                      </Link>
-                    </li>
-                  )
-                )}
+                {navLinks.map((item) => (
+                  <li key={item.id}>
+                    <Link
+                      to={`/${item.link}`}
+                      className="text-sm transition-colors duration-300 hover:text-white"
+                    >
+                      {item.text}
+                    </Link>
+                  </li>
+                ))}
               </ul>
             </div>
 
@@ -83,7 +109,7 @@ const Footer = () => {
                     key={service}
                     className="text-sm transition-colors duration-300 cursor-pointer hover:text-white"
                   >
-                    {service}
+                    <Link to="/services"> {service}</Link>
                   </li>
                 ))}
               </ul>
@@ -92,12 +118,34 @@ const Footer = () => {
 
           <div className="lg:col-span-1">
             <h3 className="mb-4 text-lg font-semibold text-white">Contact</h3>
+            <a
+              href="https://maps.app.goo.gl/MuoH2iBeW4e6u274A"
+              target="_blank"
+              className="mb-2 hover:text-white text-sm"
+            >
+              <FaLocationArrow className="inline-block" /> Business Victory
+              Solutions <br />
+              Ramalingapuram Main road,
+              <br /> Opposite Sri Venkateswara Swamy Temple,
+              <br />
+              Nellore-524003
+            </a>
             <p className="mb-2 text-sm">
-              Ramalingapuram Main road, (opposite Sri Venkateswara Swamy Temple,
-              Nellore-524 
+              {" "}
+              <a className="hover:font-bold" href="tel:+918523899330">
+                +91 8523899330
+              </a>
+              ,{" "}
+              <a className="hover:font-bold" href="tel:+917993558948">
+                +91 7993558948
+              </a>
             </p>
-            <p className="mb-2 text-sm">+91 8523899330, 7993558948</p>
-            <p className="mb-4 text-sm">bvs@gmail.com</p>
+            <a
+              href="malito:bvs@gmail.com"
+              className="mb-4 text-sm hover:font-bold"
+            >
+              bvs@gmail.com
+            </a>
 
             <h3 className="mb-4 text-lg font-semibold text-white">Connect</h3>
             <div className="flex space-x-4">
@@ -115,7 +163,11 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="pt-8 mt-8 text-center border-t border-gray-700">
+        <div className="pt-4 text-center border-t border-gray-700">
+          Icons by{" "}
+          <a href="https://icons8.com" target="_blank">
+            Icons8
+          </a>
           <p className="text-sm">
             &copy; {currentYear} BVS. All rights reserved. Empowering businesses
             with innovative solutions.
