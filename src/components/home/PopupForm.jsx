d53cc0adeb822b06e9fd5f82460c185a0df19cf3
saipradeepmorla <morlasaipradeep@gmail.com>
2024-10-23 21:09:05 +0530

    connected form with google sheet api

--- src/components/home/PopupForm.jsx
+++ src/components/home/PopupForm.jsx
@@ -1,34 +1,30 @@
 import React, { useState } from "react";
 
 const PopupForm = ({ isOpen, onClose }) => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    query: "",
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
+  const [disable, setDisable] = useState(false);
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch("http://localhost:5000/api/v1/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ ...formData }),
-    });
-    const result = await res.json();
-    console.log(result);
+    setDisable(true);
 
-    onClose();
+    const formEle = document.querySelector("form");
+    const formData = new FormData(formEle);
+    try {
+      const response = await fetch(
+        "Your google sheet apps script api",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+      const result = await response.text();
+      if (result) {
+        console.log(result);
+        onClose();
+        setDisable(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   if (!isOpen) return null;
@@ -37,28 +33,24 @@ const PopupForm = ({ isOpen, onClose }) => {
     <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center z-50">
       <div className="bg-white p-6 rounded-lg shadow-lg w-96 relative">
         <h2 className="text-xl font-semibold mb-4">Contact Form</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label className="block text-gray-700 mb-2">Full Name</label>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div className="mb-4 hidden">
+            <label className="block text-gray-700 mb-2">FormType</label>
             <input
               type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
+              name="formType"
               className="w-full p-2 border border-gray-300 rounded"
-              placeholder="Enter your full name"
+              defaultValue="visitors"
               required
             />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700 mb-2">Email</label>
+            <label className="block text-gray-700 mb-2">Full Name</label>
             <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
+              type="text"
+              name="Name"
               className="w-full p-2 border border-gray-300 rounded"
-              placeholder="Enter your email"
+              placeholder="Enter your full name"
               required
             />
           </div>
@@ -66,30 +58,30 @@ const PopupForm = ({ isOpen, onClose }) => {
             <label className="block text-gray-700 mb-2">Phone</label>
             <input
               type="tel"
-              name="phone"
-              value={formData.phone}
-              onChange={handleChange}
+              name="Phone"
               className="w-full p-2 border border-gray-300 rounded"
               placeholder="Enter your phone number"
               required
             />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700 mb-2">Any Query</label>
-            <textarea
-              name="query"
-              value={formData.query}
-              onChange={handleChange}
+            <label className="block text-gray-700 mb-2">Email</label>
+            <input
+              type="email"
+              name="Email"
               className="w-full p-2 border border-gray-300 rounded"
-              placeholder="Your query..."
+              placeholder="Enter your email"
               required
             />
           </div>
+
           <button
+            disabled={disable}
             type="submit"
             className="bg-green-700 text-white px-4 py-2 rounded-md"
           >
-            Submit
+            {" "}
+            {disable ? "submitting..." : "submit"}
           </button>
         </form>
         <button
@@ -104,8 +96,3 @@ const PopupForm = ({ isOpen, onClose }) => {
 };
 
 export default PopupForm;
-
-
-
-
-
